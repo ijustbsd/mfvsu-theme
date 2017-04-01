a357c7cd7a052384ce48bf0da8ddf555419e6ab0
ijustbsd <ijustbsd@gmail.com>
2017-04-02 01:38:01 +0300

    WP theme for math.vsu.ru

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			<p>&copy Математический факультет ВГУ - <?php printf( date('Y') ); ?></p>
+			<a href="https://www.math.vsu.ru/" title="">math.vsu.ru</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
